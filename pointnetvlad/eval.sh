1fce3009f5266c453a6adcebeff886240f48dafe
daniel <daniel.koguciuk@gmail.com>
2020-07-19 22:33:29 +0200

    With eval bash help script

--- pointnetvlad/eval.sh
+++ pointnetvlad/eval.sh
@@ -1,5 +1,5 @@
-python3 evaluate.py --log_dir log_101 --ordering 'flat,norm,squeeze=256,cg,norm' --ckpt log_101/model_after_epoch_15.ckpt --result_filename 'after_epoch_15.txt'
-python3 evaluate.py --log_dir log_101 --ordering 'flat,norm,squeeze=256,cg,norm' --ckpt log_101/model_after_epoch_16.ckpt --result_filename 'after_epoch_16.txt'
-python3 evaluate.py --log_dir log_101 --ordering 'flat,norm,squeeze=256,cg,norm' --ckpt log_101/model_after_epoch_17.ckpt --result_filename 'after_epoch_17.txt'
-python3 evaluate.py --log_dir log_101 --ordering 'flat,norm,squeeze=256,cg,norm' --ckpt log_101/model_after_epoch_18.ckpt --result_filename 'after_epoch_18.txt'
-python3 evaluate.py --log_dir log_101 --ordering 'flat,norm,squeeze=256,cg,norm' --ckpt log_101/model_after_epoch_19.ckpt --result_filename 'after_epoch_19.txt'
+#!/bin/bash
+for it in $(seq 1 1 19)
+do
+        python3 evaluate.py --log_dir log_101 --ordering 'flat,norm,squeeze=256,cg,norm' --ckpt log_101/model_after_epoch_$it.ckpt --result_filename "after_epoch_$it.txt"
+done
